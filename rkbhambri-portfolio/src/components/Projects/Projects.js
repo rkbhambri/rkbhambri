b40adaf58684151e45ff91ddf2954ff64b1ff8bb
ram <ramkrishan@kontikilabs.com>
2019-05-29 17:56:21 +0530

    Minor UI fix

--- rkbhambri-portfolio/src/components/Projects/Projects.js
+++ rkbhambri-portfolio/src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import Project from './Project/Project';
 
 const Projects = (props) => {
     return (
-        <div className="projects-list w-100 row p-5 ml-3" >
+        <div className="projects-list p-5 ml-5 row" style={{ width: '95%' }} >
             {
                 props.projects.length > 0 && props.projects.map(item => {
                     return <Project projectData={item} key={item.name} />;
